b4492f1e16b9e2a54afdba403948bd4c47203db8
Spas Cholakov <scholakov@infinigods.com>
2023-07-10 09:09:33 +0300

    product validator

--- controllers/products.controllers.go
+++ controllers/products.controllers.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"net/http"
 	"products/models"
+	"products/validationMsgHandlers"
+
 	"sync"
 
 	"github.com/gin-gonic/gin"
@@ -11,6 +13,7 @@ import (
 
 var (
 	product		models.Product
+	pmh				validationmsghandlers.ProductMsgHandler
 )
 
 type ProductControllers struct {}
@@ -20,7 +23,8 @@ func (pc *ProductControllers) CreateProduct(c *gin.Context) {
 	var wg sync.WaitGroup
 
 	if err := c.ShouldBindJSON(&product); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{ "message": err.Error() })
+		errMsg := pmh.ProductValidate(err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": errMsg})
 		return
 	}
 
@@ -96,7 +100,8 @@ func (pc *ProductControllers) UpdateProduct(c *gin.Context) {
 	var wg sync.WaitGroup
 
 	if err := c.ShouldBindJSON(&product); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{ "message": err.Error() })
+		errMsg := pmh.ProductValidate(err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": errMsg})
 		return
 	}
 
@@ -131,104 +136,3 @@ func (pc *ProductControllers) DeleteProduct(c *gin.Context) {
 	
 	c.JSON(http.StatusOK, gin.H{"message": "delete product success"})
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// package controllers
-
-// import (
-// 	"net/http"
-// 	"products/models"
-
-// 	"github.com/gin-gonic/gin"
-// )
-
-// var (
-// 	product		models.Product
-// )
-
-// type ProductControllers struct {}
-
-// func (pc *ProductControllers) CreateProduct(c *gin.Context) {
-// 	var product *models.Product
-
-// 	if err := c.ShouldBindJSON(&product); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{ "message": err.Error() })
-// 		return
-// 	}
-
-// 	err := product.CreateProductQuery(product)
-// 	if err != nil {
-// 		c.JSON(http.StatusBadGateway, gin.H{ "message": err.Error() })
-// 		return
-// 	}
-
-// 	c.JSON(http.StatusOK, gin.H{"message": "create product success"})
-// }
-
-// func (pc *ProductControllers) GetProducts(c *gin.Context) {
-// 	products, err := product.GetAllQuery()
-// 	if err != nil {
-// 		c.JSON(http.StatusBadGateway, gin.H{ "message": err.Error() })
-// 		return
-// 	}
-// 	c.JSON(http.StatusOK, products)
-// }
-
-// func (pc *ProductControllers) GetProductById(c *gin.Context) {
-// 	id := c.Param("id")
-	
-// 	productById, err := product.GetProductByIdQuery(&id)
-// 	if err != nil {
-// 		c.JSON(http.StatusBadGateway, gin.H{ "message": err.Error() })
-// 		return	
-// 	}
-
-// 	c.JSON(http.StatusOK, productById)
-// }
-
-// func (pc *ProductControllers) UpdateProduct(c *gin.Context) {
-// 	id := c.Param("id")
-// 	var product models.Product
-
-// 	if err := c.ShouldBindJSON(&product); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{ "message": err.Error() })
-// 		return
-// 	}
-
-// 	err := product.UpdateProductQuery(&id, &product)
-// 	if err != nil {
-// 		c.JSON(http.StatusBadGateway, gin.H{ "message": err.Error() })
-// 		return
-// 	}
-
-// 	c.JSON(http.StatusOK, gin.H{"message": "update product success"})
-// }
-
-// func (pc *ProductControllers) DeleteProduct(c *gin.Context) {
-// 	id := c.Param("id")
-
-// 	err := product.DeleteProductQuery(&id)
-// 	if err != nil {
-// 		c.JSON(http.StatusBadGateway, gin.H{ "message": err.Error() })
-// 		return
-// 	}
-	
-// 	c.JSON(http.StatusOK, gin.H{"message": "delete product success"})
-// }
