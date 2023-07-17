ad6d43efa6fbcced75515b86b6110fa602b36637
Spas Cholakov <scholakov@infinigods.com>
2023-07-17 09:31:02 +0300

    adding users

--- db/connect.go
+++ db/connect.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -15,12 +16,13 @@ type Db struct {
 }
 
 func (d *Db) Connect() (*mongo.Client, context.Context) {
-	ctx := context.TODO()
-
 	mongoURI := os.Getenv("MONGO_URI")
 	if mongoURI == "" {
 		log.Fatal("MONGO_URI environment variable is not set")
 	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
 	mongoconn := options.Client().ApplyURI(mongoURI)
 	mongoclient, err := mongo.Connect(ctx, mongoconn)
@@ -33,4 +35,9 @@ func (d *Db) Connect() (*mongo.Client, context.Context) {
 	fmt.Println("mongo connection established")
 
 	return mongoclient, ctx
+}
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("demo").Collection(collectionName)
+	return collection
 }
